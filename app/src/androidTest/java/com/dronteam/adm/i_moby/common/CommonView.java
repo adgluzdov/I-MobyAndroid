73267a96f0325dd75e754c66bdfef9f7a45b61ec
smbody <smboogle@gmail.com>
2016-10-18 12:37:21 +0300

    Разработана базовая арзитектура проекта.

---
+++ app/src/androidTest/java/com/dronteam/adm/i_moby/common/CommonView.java
@@ -0,0 +1,8 @@
+package com.dronteam.adm.i_moby.common;
+
+/**
+ * Created by smb on 18/10/2016.
+ */
+public interface CommonView {
+    void setViewListener(ViewListener listener);
+}
---
+++ app/src/androidTest/java/com/dronteam/adm/i_moby/common/ViewListener.java
@@ -0,0 +1,8 @@
+package com.dronteam.adm.i_moby.common;
+
+/**
+ * Created by smb on 18/10/2016.
+ */
+public interface ViewListener {
+    void OnCreateView();
+}
