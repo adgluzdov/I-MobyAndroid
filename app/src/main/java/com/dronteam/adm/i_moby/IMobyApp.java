00345aa8b165c74ae804ff793653b862932c23a7
smbody <smboogle@gmail.com>
2016-12-13 16:19:41 +0300

    Забыл добавить IMobyApp.java в коммит.

---
+++ app/src/main/java/com/dronteam/adm/i_moby/IMobyApp.java
@@ -0,0 +1,17 @@
+package com.dronteam.adm.i_moby;
+
+import android.app.Application;
+
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by smb on 13/12/2016.
+ */
+
+public class IMobyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        VKSdk.initialize(this);
+    }
+}
