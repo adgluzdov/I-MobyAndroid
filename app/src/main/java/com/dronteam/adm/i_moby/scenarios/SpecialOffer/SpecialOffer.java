a210bef23a3757665e240fe61ce8b14586e94f07
xLoSyAsHx <losyashiks200797>
2016-12-13 22:26:59 +0300

    Создал класс SpecialOffer и интерфейс SpecialOfferView

---
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/SpecialOffer/SpecialOffer.java
@@ -0,0 +1,53 @@
+package com.dronteam.adm.i_moby.scenarios.SpecialOffer;
+
+import com.dronteam.adm.i_moby.model.Item;
+
+/**
+ * Created by User on 13.12.2016.
+ */
+
+public class SpecialOffer {
+    Item item;
+
+    double discount;
+    double bonuse;
+    String additionalInfo;
+
+    public SpecialOffer(Item item) {
+        this.item = item;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getBonuse() {
+        return bonuse;
+    }
+
+    public void setBonuse(double bonuse) {
+        this.bonuse = bonuse;
+    }
+
+    public String getAdditionalInfo() {
+        if (additionalInfo == null)
+            additionalInfo = "No additional info";
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+}
