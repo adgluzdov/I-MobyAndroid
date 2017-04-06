4999e273e99b9bc52e954fb0a120eae5d7f19c7d
Andrey Gluzdov <adgluzdov@mail.ru>
2017-04-06 22:23:31 +0300

    Изменён класс VKAuth: 1)Изменен конструктор класса (Получение токена из VKSDK, Аутентификация, Авторизация) 2)Реализовано обновление токена в случае ошибки его валидации

--- app/src/main/java/com/dronteam/adm/i_moby/data/VK/VKAuth.java
+++ app/src/main/java/com/dronteam/adm/i_moby/data/VK/VKAuth.java
@@ -6,9 +6,14 @@ import android.util.Log;
 
 import com.dronteam.adm.i_moby.common.Authentication;
 import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKError;
+
+import static android.content.ContentValues.TAG;
+import static com.vk.sdk.VKSdk.LoginState.LoggedIn;
+import static com.vk.sdk.VKSdk.LoginState.LoggedOut;
 
 /**
  * Created by smb on 13/12/2016.
@@ -20,18 +25,44 @@ public class VKAuth implements Authentication {
 
     private VKAccessToken token;
 
-    public VKAuth(Activity activity) {
-        VKSdk.login(activity, "market");
+    public VKAuth(final Activity activity) {
+        // Достаём токен из SDK
+        token = VKAccessToken.currentToken();
+        // Если НЕ залогинено - Авторизуемся
+        if(!IsLoggedIn()) Authorization(activity);
+        // Если залогинено - Аутентифицируемся
+        else Authentication(activity,new VKCallback<VKSdk.LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+                // Если токен НЕ Валиден - Авторизируемся по новой
+                if(res == LoggedOut) {
+                    LogOut();
+                    Authorization(activity);
+                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        });
+    }
+
+    private void Authorization(Activity activity) {
+        VKSdk.login(activity, "market", "offline");
+    }
+
+    private void Authentication(Activity activity,VKCallback<VKSdk.LoginState> callback) {
+        VKSdk.wakeUpSession(activity,callback);
+    }
+
+    private void LogOut() {
+        VKSdk.logout();
     }
 
     @Override
     public String getToken() {
-
-        String accessToken = null;
-        if (token != null) {
-            accessToken = token.accessToken;
-        }
-        return accessToken;
+        return token.accessToken;
     }
 
     @Override
