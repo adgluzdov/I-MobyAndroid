66749546dfe7aa2437a37f3fffcc5b661de3686a
Andrey Gluzdov <adgluzdov@mail.ru>
2017-04-14 19:43:00 +0300

    Заставил MainActivity слушать VkAuth. В VKAuth убрал конструктор и перенес содержимое в отдельную функцию, потому что MainActivity.onAuth уже нужен VkAuth, однако, MainActivity.onAuth запускается из конструктора VkAuth (в случае когда уже пользователь логинился).

--- app/src/main/java/com/dronteam/adm/i_moby/common/Authentication.java
+++ app/src/main/java/com/dronteam/adm/i_moby/common/Authentication.java
@@ -1,5 +1,7 @@
 package com.dronteam.adm.i_moby.common;
 
+import android.app.Activity;
+
 /**
  * Created by smb on 13/12/2016.
  */
--- app/src/main/java/com/dronteam/adm/i_moby/data/VK/VKAuth.java
+++ app/src/main/java/com/dronteam/adm/i_moby/data/VK/VKAuth.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
+import com.dronteam.adm.i_moby.common.AuthListener;
 import com.dronteam.adm.i_moby.common.Authentication;
+import com.dronteam.adm.i_moby.common.CallBack;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKCallback;
@@ -18,16 +20,20 @@ import static com.vk.sdk.VKSdk.LoginState.LoggedOut;
 /**
  * Created by smb on 13/12/2016.
  */
+// Todo: Убрал Конструктор и создал отдельный метод, потому что в authListener.onAuth() уже нужен инициализированный класс.
 
 public class VKAuth implements Authentication {
 
     private VKAccessToken token;
+    private AuthListener authListener;
 
-    public VKAuth(Activity activity) {
+    public void auth(Activity activity, AuthListener authListener) {
+        this.authListener = authListener;
         if (!VKSdk.isLoggedIn()) {
             VKSdk.login(activity, "market", "offline");
         } else {
             token = VKAccessToken.currentToken();
+            authListener.onAuth();
         }
     }
 
@@ -41,6 +47,7 @@ public class VKAuth implements Authentication {
             @Override
             public void onResult(VKAccessToken result) {
                 token = result;
+                authListener.onAuth();
             }
             @Override
             public void onError(VKError error) {
