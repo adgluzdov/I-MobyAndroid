31c463b4e655303ec89f830da1101b1aca299582
danreegly <danreegly@gmail.com>
2017-07-12 23:17:42 +0300

    Реализован интерфейс для CatalogView

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/catalog/CatalogFragment.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/catalog/CatalogFragment.java
@@ -1,11 +1,14 @@
 package com.dronteam.adm.i_moby.scenarios.catalog;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,9 +24,20 @@ import com.dronteam.adm.i_moby.common.fragment.MainFragment;
  */
 
 public class CatalogFragment extends MainFragment implements CatalogView {
+    private int COLUMNS_COUNT = 0;
+    private SwipeRefreshLayout swipeRefreshLayout = null;
+    private  RecyclerView mRecyclerView = null;
     private LinearLayoutManager layoutManagerPhone = null;
-    private ListView getList(){
-        return getListView(R.id.list_view);
+    private GridLayoutManager layoutManagerTablet = null;
+    private View no_albums = null;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater,container,savedInstanceState);
+        swipeRefreshLayout = (SwipeRefreshLayout)getView(R.id.swipeRefreshLayout);
+        mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
+        no_albums = getView(R.id.no_albums);
+        no_albums.setVisibility(View.GONE);
+        return view;
     }
 
     @Override
@@ -33,24 +47,32 @@ public class CatalogFragment extends MainFragment implements CatalogView {
 
     @Override
     public void startTopProgressbar() {
-        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.VISIBLE);
+        swipeRefreshLayout.setRefreshing(true);
     }
 
     @Override
     public void stopTopProgressbar() {
-        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.INVISIBLE);
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public String getTitleFragment() {
-        return "dssdsd";
+        return "Каталог";
     }
 
     @Override
     public void setList(RecyclerView.Adapter adapter, Context context) {
-        RecyclerView mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
-        layoutManagerPhone = new LinearLayoutManager(context);
-        mRecyclerView.setLayoutManager(layoutManagerPhone);
-        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setHasFixedSize(true);
+        if(ScreenInfo.sizes(context).x<600) {
+            COLUMNS_COUNT = 1;
+            layoutManagerPhone = new LinearLayoutManager(context);
+            mRecyclerView.setLayoutManager(layoutManagerPhone);
+        }
+        else {
+            COLUMNS_COUNT = 2;
+            layoutManagerPhone = new GridLayoutManager(context, 2);
+            mRecyclerView.setLayoutManager(layoutManagerTablet);
+        }
+        ((RecyclerView)getView(R.id.recyclerView)).setAdapter(adapter);
     }
 }
