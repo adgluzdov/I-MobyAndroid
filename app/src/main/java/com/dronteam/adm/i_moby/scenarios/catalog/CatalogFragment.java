ad0a893c042aec889ced355baf6ae3204fc969ad
adgluzdov <adgluzdov@mail.ru>
2017-07-12 15:54:15 +0300

    1525 (#36)
    * 1499 (#33)
    
    * 1523 (#32)
    
    * Отрефакторено ShowCaseView
    
    * Реализация интерфейса ShowCaseView (#27)
    
    * Реализация интерфейса ShowCaseView
    
    * Fix context
    
    * Добавил новые методы из ShowCaseView. Устранил ошибку: когда второй раз вызываются Item у адаптера, то они не заполняются. Исправил, переписав CommonRecicleViewAdapter. (#30)
    
    * Устранил неполадку: Presenter узнаёт onCreate из MainFragment, но в View, которые наследуются от MainFragment, могут переопределять метод onCreate. Реализовано, через предупреждение презентера об onCreate в методе onViewCreated. (#31)
    
    * Рефакторинг CatalogView. Изменен адаптер. Убран обработчик кнопки.
    
    * В catalogPresenter изменен адаптер. Убрана логика для кнопки. (#34)
    
    * Написан AllGoodsPresentr.

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/catalog/CatalogFragment.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/catalog/CatalogFragment.java
@@ -1,5 +1,10 @@
 package com.dronteam.adm.i_moby.scenarios.catalog;
 
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -8,6 +13,7 @@ import android.widget.ProgressBar;
 
 import com.dronteam.adm.i_moby.R;
 import com.dronteam.adm.i_moby.common.CallBack;
+import com.dronteam.adm.i_moby.common.ScreenInfo;
 import com.dronteam.adm.i_moby.common.fragment.MainFragment;
 
 /**
@@ -15,41 +21,36 @@ import com.dronteam.adm.i_moby.common.fragment.MainFragment;
  */
 
 public class CatalogFragment extends MainFragment implements CatalogView {
+    private LinearLayoutManager layoutManagerPhone = null;
     private ListView getList(){
         return getListView(R.id.list_view);
-    }
-    @Override
-    public void setList(BaseAdapter adapter) {
-        getList().setAdapter(adapter);
-    }
-
-    @Override
-    public void setOnButtonMainAlbumClick(final CallBack callBack) {
-        ((Button)getView(R.id.button_open_main_album)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                callBack.call();
-            }
-        });
     }
 
     @Override
     protected int getLayout() {
-        return R.layout.catalog;
+        return R.layout.catalog2;
     }
 
     @Override
     public void startTopProgressbar() {
-        ((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.VISIBLE);
+        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.VISIBLE);
     }
 
     @Override
     public void stopTopProgressbar() {
-        ((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.INVISIBLE);
+        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.INVISIBLE);
     }
 
     @Override
     public String getTitleFragment() {
         return "dssdsd";
+    }
+
+    @Override
+    public void setList(RecyclerView.Adapter adapter, Context context) {
+        RecyclerView mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
+        layoutManagerPhone = new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(layoutManagerPhone);
+        mRecyclerView.setAdapter(adapter);
     }
 }
