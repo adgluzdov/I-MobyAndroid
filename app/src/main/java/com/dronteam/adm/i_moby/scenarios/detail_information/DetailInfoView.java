a5d0c613e26efb10c6562960e51f4c579e60e5b7
Andrey Gluzdov <adgluzdov@mail.ru>
2017-07-13 16:05:13 +0300

    ОтреФакторено DetailInfoView. Унаследовано от ViewWithToolbar. Добавлены новые методы.

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/detail_information/DetailInfoView.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/detail_information/DetailInfoView.java
@@ -5,20 +5,22 @@ import android.graphics.Bitmap;
 import android.view.View;
 
 import com.dronteam.adm.i_moby.common.CommonView;
+import com.dronteam.adm.i_moby.common.fragment.with_toolbar.ViewWithToolbar;
+import com.dronteam.adm.i_moby.model.product.Category;
+import com.dronteam.adm.i_moby.model.product.Price;
 
 /**
  * Created by adm on 27.04.2017.
  */
 
-public interface DetailInfoView extends CommonView {
+public interface DetailInfoView extends CommonView,ViewWithToolbar {
+    void setImage(Bitmap image);
+    void setPlaceHolder(int resId);
     void setTitle(String title);
-    void setDescription(String description);
-    void setPrice(String price);
-    void setCategorySection(String categorySection);
-    void setCategoryName(String categoryName);
-    void setDate(String date);
-    void setBlankImage();
-    void setImage(Bitmap bitmap);
+    void setCategoty(Category category);
+    void setDate(Integer date);
+    void setPrice(Price price);
+
     void setEditListener(View.OnClickListener listener);
     void informingMessageIsSent(Context context);
     void informingMessageIsNotSent(Context context);
