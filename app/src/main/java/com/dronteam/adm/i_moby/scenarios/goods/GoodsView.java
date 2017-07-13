f6ecc1be81ee2c79ab97013d8fd97600be606f4a
Andrey Gluzdov <adgluzdov@mail.ru>
2017-07-13 20:40:59 +0300

    Отрефакторил GoodsView: унаследовал от view...withSearchView, поменял adapter, добавил setOnScrollViewListener(OnScroolViewListener listener);

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/goods/GoodsView.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/goods/GoodsView.java
@@ -1,9 +1,12 @@
 package com.dronteam.adm.i_moby.scenarios.goods;
 
+import android.support.v7.widget.RecyclerView;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 
 import com.dronteam.adm.i_moby.common.CommonView;
+import com.dronteam.adm.i_moby.common.OnScrollViewListener;
+import com.dronteam.adm.i_moby.common.fragment.with_toolbar.with_menu.with_search_view.ViewWithToolbarWithSearchView;
 import com.dronteam.adm.i_moby.common.progressbar.TopProgressbarView;
 import com.dronteam.adm.i_moby.common.progressbar.UnderProgressbarView;
 
@@ -11,11 +14,7 @@ import com.dronteam.adm.i_moby.common.progressbar.UnderProgressbarView;
  * Created by smb on 18/10/2016.
  */
 
-public interface GoodsView extends CommonView,TopProgressbarView,UnderProgressbarView {
-    void setList(BaseAdapter adapter);
-
-    void setOnScrollListener(AbsListView.OnScrollListener listener);
-    int listViewGetLastVisiblePosition();
-    int listViewGetHeaderViewsCount();
-    int listViewGetFooterViewsCount();
+public interface GoodsView extends CommonView,TopProgressbarView,ViewWithToolbarWithSearchView {
+    void setList(RecyclerView.Adapter adapter);
+    void setOnScrollListener(OnScrollViewListener listener);
 }
