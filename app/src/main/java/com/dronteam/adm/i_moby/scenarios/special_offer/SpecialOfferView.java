3d24c94a4511df39787c25515961f90c1a34e658
Andrey Gluzdov <adgluzdov@mail.ru>
2017-07-13 14:12:59 +0300

    ОтреФакторен SpecialOfferView: унаследован от ProductView, добавлен метод setTags(Tags tags);

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/special_offer/SpecialOfferView.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/special_offer/SpecialOfferView.java
@@ -4,19 +4,13 @@ import android.graphics.Bitmap;
 import android.view.View;
 
 import com.dronteam.adm.i_moby.common.adapters.ItemView;
+import com.dronteam.adm.i_moby.model.special_offer.Tags;
+import com.dronteam.adm.i_moby.scenarios.product.ProductView;
 
 /**
  * Created by User on 13.12.2016.
  */
 
-public interface SpecialOfferView extends ItemView {
-    void setDiscount(String discount);
-    void setBonus(String bonus);
-    void setAdditionalInfo(String additionalInfo);
-    void setImage(Bitmap image);
-    void setImage(int resId);
-    void setTitle(String title);
-    void setPrice(String price);
-
-    void setEditListener(View.OnClickListener listener);
+public interface SpecialOfferView extends ProductView,ItemView {
+    void setTags(Tags tags);
 }
