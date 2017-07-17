96ff2c3f77290b166fcdff3a0c7f7a1defe6ec68
danreegly <danreegly@gmail.com>
2017-07-17 11:19:20 +0300

    Реализация интерфейса для GoodsView

--- app/src/main/java/com/dronteam/adm/i_moby/scenarios/goods/GoodsFragment.java
+++ app/src/main/java/com/dronteam/adm/i_moby/scenarios/goods/GoodsFragment.java
@@ -1,9 +1,15 @@
 package com.dronteam.adm.i_moby.scenarios.goods;
 
 
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -21,7 +27,23 @@ import com.dronteam.adm.i_moby.common.fragment.with_toolbar.with_menu.with_searc
 
 public class GoodsFragment extends FragmentWithToolbarWithSearchView implements GoodsView {
 
-    LinearLayoutManager layoutManagerPhone;
+    private int COLUMNS_COUNT = 0;
+    private SwipeRefreshLayout swipeRefreshLayout = null;
+    private  RecyclerView mRecyclerView = null;
+    private LinearLayoutManager layoutManagerPhone = null;
+    private GridLayoutManager layoutManagerTablet = null;
+    private View no_goods = null;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater,container,savedInstanceState);
+        swipeRefreshLayout = (SwipeRefreshLayout)getView(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
+        no_goods = getView(R.id.no_goods);
+        no_goods.setVisibility(View.GONE);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        return view;
+    }
     @Override
     protected int getLayout() {
         return R.layout.goods2;
@@ -29,30 +51,37 @@ public class GoodsFragment extends FragmentWithToolbarWithSearchView implements 
 
     @Override
     public void startTopProgressbar() {
-        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.VISIBLE);
+        swipeRefreshLayout.setRefreshing(true);
     }
 
     @Override
     public void stopTopProgressbar() {
-        //((ProgressBar)getView(R.id.progress_bar)).setVisibility(ProgressBar.INVISIBLE);
+        swipeRefreshLayout.setRefreshing(false);
     }
     @Override
     public void setList(RecyclerView.Adapter adapter, ViewManager viewManager) {
-        RecyclerView mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
-        layoutManagerPhone = new LinearLayoutManager(viewManager.getContext());
-        mRecyclerView.setLayoutManager(layoutManagerPhone);
+        if(ScreenInfo.sizes(viewManager.getContext()).x<600) {
+            COLUMNS_COUNT = 1;
+            layoutManagerPhone = new LinearLayoutManager(viewManager.getContext());
+            mRecyclerView.setLayoutManager(layoutManagerPhone);
+        }
+        else {
+            COLUMNS_COUNT = 2;
+            layoutManagerPhone = new GridLayoutManager(viewManager.getContext(), 2);
+            mRecyclerView.setLayoutManager(layoutManagerTablet);
+        }
         ((RecyclerView)getView(R.id.recyclerView)).setAdapter(adapter);
     }
 
     @Override
     public void notifyNoGoods() {
-
+        mRecyclerView.setVisibility(View.GONE);
+        no_goods.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void setOnScrollListener(final OnScrollViewListener listener) {
-        RecyclerView mRecyclerView = (RecyclerView)getView(R.id.recyclerView);
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
@@ -64,12 +93,18 @@ public class GoodsFragment extends FragmentWithToolbarWithSearchView implements 
 
     @Override
     public int getChildCount() {
-        return layoutManagerPhone.getChildCount();
+        if(COLUMNS_COUNT == 1)
+            return layoutManagerPhone.getChildCount();
+        else
+            return layoutManagerTablet.getChildCount();
     }
 
     @Override
     public int findFirstVisibleItemPosition() {
-        return layoutManagerPhone.findFirstVisibleItemPosition();
+        if(COLUMNS_COUNT == 1)
+            return layoutManagerPhone.findFirstVisibleItemPosition();
+        else
+            return layoutManagerTablet.findFirstVisibleItemPosition();
     }
 
     @Override
@@ -84,7 +119,7 @@ public class GoodsFragment extends FragmentWithToolbarWithSearchView implements 
 
     @Override
     public String getTitle() {
-        return "Главная";
+        return "Товары";
     }
 
     @Override
